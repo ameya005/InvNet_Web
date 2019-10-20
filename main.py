de9ee147fdf4d7ecc2e65644c877dba3f56c080f
ameya005 <ameya@iastate.edu>
2019-10-20 15:48:50 -0500

    added procfile and runtime

--- main.py
+++ main.py
@@ -210,5 +210,5 @@ def server_error(e):
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True, threaded=True)
 # [END gae_flex_storage_app]
